7c441ae2c41713b68fac44904ec72e6a01a773a7
kyum22n <aphrod207@naver.com>
2025-08-13 19:57:41 +0900

    [Bronze V] Title: 윤년, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    int year = scanner.nextInt();
+    
+    if((1 <= year) && (year <= 4000)) {
+      
+      if( ((year % 4) == 0) && ((year % 100) != 0) || ((year % 400) == 0) ) {
+        System.out.println("1");
+      } else {
+        System.out.println("0");
+      }
+    }
+    
+
+  }
+}
